be23ac75f0eee0a9a5b395a452fd614f264b8dc5
Irene Jiaxin Fan <irene0206@outlook.com>
2023-02-15 22:06:29 +0000

    Workbook1 Task4

--- Weekly Workbooks/04 MVC and Collections/cw-oxo/src/main/java/edu/uob/OXOController.java
+++ Weekly Workbooks/04 MVC and Collections/cw-oxo/src/main/java/edu/uob/OXOController.java
@@ -7,12 +7,26 @@ public class OXOController {
         gameModel = model;
     }
 
-    public void handleIncomingCommand(String command) throws OXOMoveException {}
+    public void handleIncomingCommand(String command) throws OXOMoveException {
+        String com = command.toLowerCase();
+        int row = com.charAt(0) - 'a';
+        int col = Character.getNumericValue(com.charAt(1)) - 1;
+        if (gameModel.getCellOwner(row, col) == null) {
+            gameModel.setCellOwner(row, col, gameModel.getPlayerByNumber(gameModel.getCurrentPlayerNumber()));
+            gameModel.setCurrentPlayerNumber(1 - gameModel.getCurrentPlayerNumber());
+        }
+    }
     public void addRow() {}
     public void removeRow() {}
     public void addColumn() {}
     public void removeColumn() {}
     public void increaseWinThreshold() {}
     public void decreaseWinThreshold() {}
-    public void reset() {}
+    public void reset() {
+        for (int i = 0; i < gameModel.getNumberOfRows(); i++) {
+            for (int j = 0; j < gameModel.getNumberOfColumns(); j++) {
+                gameModel.setCellOwner(i, j, null);
+            }
+        }
+    }
 }
