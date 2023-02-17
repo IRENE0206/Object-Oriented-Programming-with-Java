59c89404525e7fc9870726dd872d8c02b208bf03
Irene Jiaxin Fan <irene0206@outlook.com>
2023-02-17 01:30:54 +0000

    Workshop1 Task5

--- Weekly Workbooks/04 MVC and Collections/cw-oxo/src/main/java/edu/uob/OXOModel.java
+++ Weekly Workbooks/04 MVC and Collections/cw-oxo/src/main/java/edu/uob/OXOModel.java
@@ -1,8 +1,10 @@
 package edu.uob;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OXOModel {
 
-    private OXOPlayer[][] cells;
+    private List<List<OXOPlayer>> cells;
     private OXOPlayer[] players;
     private int currentPlayerNumber;
     private OXOPlayer winner;
@@ -11,7 +13,14 @@ public class OXOModel {
 
     public OXOModel(int numberOfRows, int numberOfColumns, int winThresh) {
         winThreshold = winThresh;
-        cells = new OXOPlayer[numberOfRows][numberOfColumns];
+        cells = new ArrayList<>(numberOfRows);
+        for (int i = 0; i < numberOfRows; i++) {
+            List<OXOPlayer> row = new ArrayList<>(numberOfColumns);
+            for (int j = 0; j < numberOfColumns; j++) {
+                row.add(null);
+            }
+            cells.add(row);
+        }
         players = new OXOPlayer[2];
     }
 
@@ -49,19 +58,19 @@ public class OXOModel {
     }
 
     public int getNumberOfRows() {
-        return cells.length;
+        return cells.size();
     }
 
     public int getNumberOfColumns() {
-        return cells[0].length;
+        return cells.get(0).size();
     }
 
     public OXOPlayer getCellOwner(int rowNumber, int colNumber) {
-        return cells[rowNumber][colNumber];
+        return cells.get(rowNumber).get(colNumber);
     }
 
     public void setCellOwner(int rowNumber, int colNumber, OXOPlayer player) {
-        cells[rowNumber][colNumber] = player;
+        cells.get(rowNumber).set(colNumber, player);
     }
 
     public void setWinThreshold(int winThresh) {
@@ -80,4 +89,40 @@ public class OXOModel {
         return gameDrawn;
     }
 
+    public void addColumn() {
+        for (int i = 0; i < getNumberOfRows(); i++) {
+            cells.get(i).add(null);
+        }
+    }
+
+    public void addRow() {
+        int col = getNumberOfColumns();
+        List<OXOPlayer> row = new ArrayList<>(col);
+        for (int i = 0; i < col; i++) {
+            row.add(null);
+        }
+        cells.add(row);
+    }
+
+    public void removeColumn() {
+        int colIndex = getNumberOfColumns() - 1;
+        for (int i = 0; i < getNumberOfRows(); i++) {
+            if (cells.get(i).get(colIndex) != null) {
+                return;
+            }
+        }
+        for (int i = 0; i < getNumberOfRows(); i++) {
+            cells.get(i).remove( colIndex);
+        }
+    }
+
+    public void removeRow() {
+        int rowIndex = getNumberOfRows() - 1;
+        for (int i = 0; i < getNumberOfColumns(); i++) {
+            if (cells.get(rowIndex).get(i) != null) {
+                return;
+            }
+        }
+        cells.remove(rowIndex);
+    }
 }
